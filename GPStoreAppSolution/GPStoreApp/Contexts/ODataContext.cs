0b103443fae8b013a86e7301c9ae037b8543cced
miltonc <milton.chandradas@sap.com>
2015-01-08 17:35:47 -0500

    Initial commit
    Windows sample solution source files

---
+++ GPStoreAppSolution/GPStoreApp/Contexts/ODataContext.cs
@@ -0,0 +1,166 @@
+﻿using SAP.Data.OData;
+using SAP.Data.OData.Store;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Windows.UI.Xaml;
+
+namespace Contexts {
+	public class ODataContext : INotifyPropertyChanged {
+		// methods
+		public ODataContext() {
+			this.ServiceUrl = "http://services.odata.org/V3/OData/OData.svc/";
+			//this.Store = new SAP.Data.OData.Online.Store.ODataStore(serviceUrl);
+			this.IsStoreCreated = false;
+		}
+		public void InitializeStore() {
+			this.Store = new SAP.Data.OData.Online.Store.ODataStore(this.ServiceUrl);
+		}
+
+		private void NotifyPropertyChanged(string propertyName) {
+			if (PropertyChanged != null)
+				PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+		}
+		public async System.Threading.Tasks.Task DownloadCollection(string CollectionName) {
+			try {
+				this.RingVisible = Visibility.Visible;
+				var execution = Store.ScheduleReadEntitySet(CollectionName);
+				var response = await execution.Response;
+				this.EntitySet = (SAP.Data.OData.Online.ODataEntitySet)((IODataResponseSingle)response).Payload;
+				this.RingVisible = Visibility.Collapsed;
+			} catch (Exception) {
+				this.RingVisible = Visibility.Collapsed;
+				throw;
+			}
+		}
+
+		public async System.Threading.Tasks.Task DownloadRelatedItems(string navigationPropertyName) {
+			if (this.Entity == null) {
+				this.RingVisible = Visibility.Collapsed;
+				throw new Exception("There is no entity in the context.");
+			}
+
+			IODataNavigationProperty navigationProperty = this.entity.GetNavigationProperty(navigationPropertyName);
+
+			try {
+				this.RingVisible = Visibility.Visible;
+				var execution = Store.ScheduleRequest(new SAP.Data.OData.Store.ODataRequestParametersSingle(navigationProperty.AssociationResourcePath));
+				var response = await execution.Response;
+				if (response is IODataResponseSingle) {
+					if (((IODataResponseSingle)response).PayloadType == SAP.Data.OData.ODataType.EntitySet) {
+						this.RelatedEntitySet = (SAP.Data.OData.Online.ODataEntitySet)((IODataResponseSingle)response).Payload;
+					}
+				}
+				//if (response.PayloadType == SAP.Data.OData.ODataPayloadType.EntitySet) {
+				//	this.EntitySet = (SAP.Data.OData.Online.ODataEntitySet)response;
+				//}
+				this.RingVisible = Visibility.Collapsed;
+			} catch (Exception) {
+				this.RingVisible = Visibility.Collapsed;
+				throw;
+			}
+		}
+
+		// events
+		public event PropertyChangedEventHandler PropertyChanged;
+		public event EventHandler OperationFinished;
+
+		// properties
+		public SAP.Data.OData.Online.Store.ODataStore Store { get; set; }
+
+		private string serviceUrl;
+
+		public string ServiceUrl {
+			get { return serviceUrl; }
+			set {
+				serviceUrl = value;
+				NotifyPropertyChanged("ServiceUrl");
+			}
+		}
+		
+
+		private bool isStoreCreated = false;
+		public bool IsStoreCreated {
+			get { return isStoreCreated; }
+			set {
+				isStoreCreated = value;
+				NotifyPropertyChanged("IsStoreCreated");
+			}
+		}
+
+		protected SAP.Data.OData.Online.ODataEntitySet entitySet;
+		public SAP.Data.OData.Online.ODataEntitySet EntitySet {
+			get { return entitySet; }
+			set {
+				entitySet = value;
+				NotifyPropertyChanged("EntitySet");
+				NotifyPropertyChanged("EntitySetCount");
+			}
+		}
+
+
+		IEnumerable<string> entitySetNames = null;
+		public IEnumerable<string> EntitySetNames {
+			get {
+				return entitySetNames;
+			}
+
+			set {
+				entitySetNames = value;
+				NotifyPropertyChanged("EntitySetNames");
+			}
+		}
+
+		public string EntitySetCount {
+			get {
+				return this.entitySet == null ? "(0)" : "(" + this.entitySet.Count + ")";
+			}
+		}
+
+		protected SAP.Data.OData.IODataEntity entity;
+		public SAP.Data.OData.IODataEntity Entity {
+			get { return this.entity; }
+			set {
+				this.entity = value;
+				NotifyPropertyChanged("Entity");
+			}
+		}
+
+		protected SAP.Data.OData.Online.ODataEntitySet relatedEntitySet;
+		public SAP.Data.OData.Online.ODataEntitySet RelatedEntitySet {
+			get { return relatedEntitySet; }
+			set {
+				relatedEntitySet = value;
+				NotifyPropertyChanged("RelatedEntitySet");
+			}
+		}
+
+		private Visibility ringVisible = Visibility.Collapsed;
+		public Visibility RingVisible {
+			get { return this.ringVisible; }
+			set {
+				this.ringVisible = value;
+				this.CanEditItems = (((Visibility)value) == Visibility.Visible) ? false : true;
+				if ((this.CanEditItems) && (OperationFinished != null)) OperationFinished(this, null);
+				NotifyPropertyChanged("RingVisible");
+			}
+		}
+
+		private bool canEditItems = false;
+		public bool CanEditItems {
+			get { return canEditItems; }
+			set {
+				canEditItems = value;
+				NotifyPropertyChanged("CanEditItems");
+			}
+		}
+
+
+		public bool IsInEditMode { get; set; }
+	}
+
+	public class SharedContext {
+		public static ODataContext Context { get; set; }
+	}
+}
---
+++ GPStoreAppSolution/GPStoreApp/MainPage.xaml.cs
@@ -0,0 +1,94 @@
+﻿using Contexts;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace GPStoreApp {
+	public class TemplateSelector : DataTemplateSelector {
+		public DataTemplate SimplePropertyTemplate { get; set; }
+		public DataTemplate ComplexPropertyTemplate { get; set; }
+
+		protected override DataTemplate SelectTemplateCore(object item, DependencyObject container) {
+			var property = item as SAP.Data.OData.IODataProperty;
+			if (property != null) {
+				return (property.IsComplex) ? ComplexPropertyTemplate : SimplePropertyTemplate;
+			}
+
+			return base.SelectTemplateCore(item, container);
+		}
+	}
+
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MainPage : Page {
+		public MainPage() {
+			this.InitializeComponent();
+
+			this.DataContext = SharedContext.Context = new ODataContext();
+		}
+
+		private async void InitializeContext() {
+			string message = null;
+			try {
+				SharedContext.Context.InitializeStore();
+				SharedContext.Context.RingVisible = Visibility.Visible;
+				var client = new SAP.Net.Http.HttpClient(
+					new System.Net.Http.HttpClientHandler() {
+						Credentials = new System.Net.NetworkCredential(string.IsNullOrEmpty(TB_User.Text) ? null : TB_User.Text, 
+																	   string.IsNullOrEmpty(TB_Password.Password) ? null : TB_Password.Password)
+					}, true);
+				await SharedContext.Context.Store.OpenAsync(client);
+				SharedContext.Context.IsStoreCreated = true;
+				SharedContext.Context.EntitySetNames = SharedContext.Context.Store.Metadata.GetMetaEntityContainer(SharedContext.Context.Store.Metadata.MetaEntityContainerNames.First()).EntitySetNames;
+			} catch (Exception ex) {
+				message = ex.Message;
+			}
+			SharedContext.Context.RingVisible = Visibility.Collapsed;
+
+			if (message != null)
+				await (new Windows.UI.Popups.MessageDialog("We could not initialize the store. Details: " + message).ShowAsync());
+		}
+
+		private async void LB_Collections_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+			string message = null;
+			try {
+				string collectionName = (string)((ListBox)sender).SelectedItem;
+				if (collectionName == null) SharedContext.Context.EntitySet = null;
+				else await SharedContext.Context.DownloadCollection(collectionName);
+			} catch (Exception ex) {
+				message = ex.Message;
+			}
+
+			SharedContext.Context.RingVisible = Visibility.Collapsed;
+
+			if (message != null)
+				await(new Windows.UI.Popups.MessageDialog("We could not initialize the store. Details: " + message).ShowAsync());
+		}
+
+		private void Button_ConnectClick(object sender, RoutedEventArgs e) {
+			Popup_Settings.IsOpen = false;
+			InitializeContext();
+		}
+
+		private void Page_SizeChanged(object sender, SizeChangedEventArgs e) {
+			Grid_Settings.Width = this.ActualWidth;
+			Grid_Settings.Height = this.ActualHeight;
+		}
+
+
+	}
+}
